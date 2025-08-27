b14fcf2973b5a60cb810c638fd8708dbfadd406a
Suvra Deb Paul <suvra.deb.paul@gmail.com>
2025-08-27 13:56:54 +0600

    Card Functionality Done

--- scripts.js
+++ scripts.js
@@ -3,6 +3,8 @@ const coinCount = document.getElementById("coin-count");
 const copyCount = document.getElementById("copy-count");
 const allHearts = document.querySelectorAll(".fa-heart");
 const allCallButtons = document.querySelectorAll(".btn-call");
+const historyCard = document.getElementById("history-card");
+const clearHistory = document.getElementById("clear-history");
 let totalHeart = 0;
 
 allHearts.forEach((heart) => {
@@ -13,7 +15,37 @@ allHearts.forEach((heart) => {
 });
 
 allCallButtons.forEach((callBtn) => {
-  callBtn.addEventListener("click", () => {
-    alert();
+  callBtn.addEventListener("click", (e) => {
+    const card = e.target.closest(".card");
+    const cardHeading = card.querySelector(".card-heading").innerText;
+    const cardNumber = card.querySelector(".card-number").innerText;
+    let coinValue = parseInt(coinCount.innerText);
+    if (coinValue > 0) {
+      alert(`📞 Calling... ${cardHeading} ${cardNumber}`);
+      coinValue -= 20;
+      coinCount.innerText = coinValue;
+      const now = new Date();
+      const timeStr = now.toLocaleTimeString();
+      historyCard.insertAdjacentHTML(
+        "beforeend",
+        `
+          <div
+            class="flex justify-between items-center px-4 py-2 my-2 bg-gray-100 rounded-xl"
+          >
+            <div class = "basis-2/3">
+              <h4 class="font-bold py-1">${cardHeading}</h4>
+              <p class="text-sm text-gray-600">${cardNumber}</p>
+            </div>
+            <div>
+              <p class="text-sm text-gray-600">${timeStr}</p>
+            </div>
+          </div> 
+          `
+      );
+    } else {
+      alert(
+        "❌ You Dont Have Sufficient Balance To Call. You Need Minimum 20 Coins To Call"
+      );
+    }
   });
 });
